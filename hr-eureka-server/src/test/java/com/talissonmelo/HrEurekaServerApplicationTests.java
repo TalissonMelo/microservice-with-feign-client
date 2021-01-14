fcdbf2325e262193d97859c13e38bddda0b7f21c
TalissonMelo <talissonmelorodrigues@gmail.com>
2021-01-14 19:47:12 -0300

    Criado eureka server

---
+++ hr-eureka-server/src/test/java/com/talissonmelo/HrEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.talissonmelo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
