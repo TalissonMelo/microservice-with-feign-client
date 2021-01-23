8fed9f0ca4c0303299c5625b8135dcc000bf850c
TalissonMelo <talissonmelorodrigues@gmail.com>
2021-01-23 07:56:36 -0300

    Configurando clientes Eureka-Server

--- hr-payroll/src/main/java/com/talissonmelo/HrPayrollApplication.java
+++ hr-payroll/src/main/java/com/talissonmelo/HrPayrollApplication.java
@@ -2,8 +2,10 @@ package com.talissonmelo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableEurekaClient
 @SpringBootApplication
 @EnableFeignClients
 public class HrPayrollApplication {
